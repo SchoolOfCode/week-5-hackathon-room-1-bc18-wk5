94af474f05fac60a1e344c3006f5ffcedc0eb973
fergus-g <fergus.gildea@utrack.com>
2025-01-09 15:36:25 +0000

    Added leaderboard functionality

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -2,8 +2,8 @@ import { fetchLeaderboard } from "../models/leaderboard.js";
 
 export async function getLeaderboard(req, res) {
   try {
-    const Players = await fetchLeaderboard();
-    res.status(200).json({ status: "success", data: Players });
+    const leaderBoard = await fetchLeaderboard();
+    res.status(200).json({ status: "success", data: leaderBoard });
   } catch (error) {
     res.status(500).json({ status: "error", message: error.message });
   }
--- models/leaderboard.js
+++ models/leaderboard.js
@@ -1,3 +1,8 @@
 import { pool } from "../db/index.js";
 
-export async function fetchLeaderboard() {}
+export async function fetchLeaderboard() {
+  const result = await pool.query(
+    "SELECT player_name, average_winrate FROM player ORDER BY average_winrate DESC"
+  );
+  return result.rows;
+}
