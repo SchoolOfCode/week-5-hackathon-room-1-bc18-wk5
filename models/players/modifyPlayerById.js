4010d87431167464f130b4b700a3e6885e005851
fergus-g <fergus.gildea@utrack.com>
2025-01-14 21:04:18 +0000

    Deploy to Heroku

--- models/players/modifyPlayerById.js
+++ models/players/modifyPlayerById.js
@@ -7,7 +7,7 @@ export default async function modifyPlayerById(
   best_map
 ) {
   const result = await pool.query(
-    "UPDATE players SET player_name = $1, favourite_weapon = $2, best_map = $3,  WHERE id = $4 RETURNING *",
+    "UPDATE players SET player_name = $1, favourite_weapon = $2, best_map = $3  WHERE id = $4 RETURNING *",
     [player_name, favourite_weapon, best_map, id]
   );
   return result.rows;
