1e65385586d0c4f9168621af03978a3257430b1c
fergus-g <fergus.gildea@utrack.com>
2025-01-15 16:56:17 +0000

    Deploy to Heroku

--- models/players/modifyPlayerKill.js
+++ models/players/modifyPlayerKill.js
@@ -3,14 +3,14 @@ import { pool } from "../../db/index.js";
 export default async function modifyPlayerKill(id) {
   try {
     const result = await pool.query(
-      "UPDATE players SET kills = COALESCE(wins, 0) + 1 WHERE id = $1 RETURNING *",
+      "UPDATE players SET kills = kills + 1 WHERE id = $1 RETURNING *",
       [id]
     );
     if (result.rows.length === 0) {
       throw new Error("Player not found");
     }
 
-    return result.rows;
+    return result.rows[0]; // Return the updated player object
   } catch (error) {
     throw new Error(error.message);
   }
